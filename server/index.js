f5aa0495c9db1ae0f78a42fc77ca90b59d4c3cff
sathiswar <90893542+sathiswarj@users.noreply.github.com>
2025-10-28 18:23:03 +0530

    Refactor CORS and server listening logic
    Updated CORS configuration to allow specific frontend URLs and refactored server listening port logic.

--- server/index.js
+++ server/index.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const authRoutes = require("./router/authRoutes")
-const movieRoutes = require("./router/movieRoutes")
+const authRoutes = require("./router/authRoutes");
+const movieRoutes = require("./router/movieRoutes");
 const app = express();
- const dotenv = require("dotenv")
+const dotenv = require("dotenv");
 dotenv.config();
 
-app.use(cors());
+ app.use(cors({
+    origin: ['https://ott-project.onrender.com', 'http://localhost:3000'], // Add frontend URLs
+    credentials: true
+}));
+
 app.use(express.json());
-app.use("/api", authRoutes)
+app.use("/api", authRoutes);
 app.use("/api/movies", movieRoutes);
 
 mongoose.connect(process.env.MONGODB_URI, {
@@ -23,7 +27,7 @@ mongoose.connect(process.env.MONGODB_URI, {
         console.error("MongoDB connection failed:", error.message);
     });
 
-
-app.listen(process.env.PORT, () => {
-    console.log(`Server running on http://localhost:${process.env.PORT}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
